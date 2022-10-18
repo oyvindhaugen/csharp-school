8165d1b11e2e419bef9d54b10d17324e91a97a52
Øyvind Haugen <oyvindet@gmail.com>
2022-10-18 11:20:35 +0200

    dsadas

--- Clock/Program.cs
+++ Clock/Program.cs
@@ -1,82 +1,90 @@
-﻿Console.WriteLine("Enter current second: ");
+﻿//Here I get the time parameters for Clock
+Console.WriteLine("Enter current second: ");
 string? usrSecond = Console.ReadLine();
 Console.WriteLine("Enter current minute: ");
 string? usrMinute = Console.ReadLine();
 Console.WriteLine("Enter current hour: ");
 string? usrHour = Console.ReadLine();
 Console.Clear();
+//Here I initialize clock as a new Clock() object
 Clock clock = new Clock(Convert.ToInt32(usrSecond), Convert.ToInt32(usrMinute), Convert.ToInt32(usrHour));
-Console.WriteLine("Index: \nExit: 0\nPrint hour: 1\nPrint minute: 2\nPrint second: 3\nSet hour: 4\nSet minute: 5\nSet second: 6\nAdd hour: 7\nAdd minute: 8\nAdd second: 9\nPrint time: 10\nAdd second: 11\n");
+Console.WriteLine("Index: \nExit: 0\nPrint hour: 1\nPrint minute: 2\nPrint second: 3\nPrint time: 4\nSet hour: 5\nSet minute: 6\nSet second: 7\nAdd hour(s): 8\nAdd minute(s): 9\nAdd second(s): 10\nTick: 11\n");
+//Here is a while loop that just keeps going until exited, which listens for commands in console
 while (true)
 {
     string? usrInput = Console.ReadLine();
-    if (Convert.ToInt32(usrInput) == 0)
+    //Here it checks the input and if it matches any of the cases it executes the code within that case, if not, it defaults to an error message
+    switch (usrInput)
     {
-        Environment.Exit(0);
-    }
-    else if (Convert.ToInt32(usrInput) == 1)
-    {
-        Console.WriteLine(clock.getHour() + " hour(s)");
-    }
-    else if (Convert.ToInt32(usrInput) == 2)
-    {
-        Console.WriteLine(clock.getMinute() + " minute(s)");
-    }
-    else if (Convert.ToInt32(usrInput) == 3)
-    {
-        Console.WriteLine(clock.getSecond() + " second(s)");
-    }
-    else if (Convert.ToInt32(usrInput) == 4)
-    {
-        Console.WriteLine("Enter hour you wish to set it to: ");
-        usrInput = Console.ReadLine();
-        clock.setHour(Convert.ToInt32(usrInput));
-        Console.WriteLine($"Hour is now set to: {clock.getHour()}");
-    }
-    else if (Convert.ToInt32(usrInput) == 5)
-    {
-        Console.WriteLine("Enter minute you wish to set it to: ");
-        usrInput = Console.ReadLine();
-        clock.setMinute(Convert.ToInt32(usrInput));
-        Console.WriteLine($"Minute is now set to: {clock.getMinute()}");
-    }
-    else if (Convert.ToInt32(usrInput) == 6)
-    {
-        Console.WriteLine("Enter second you wish to set it to: ");
-        usrInput = Console.ReadLine();
-        clock.setSecond(Convert.ToInt32(usrInput));
-        Console.WriteLine($"Second is now set to: {clock.getSecond()}");
-    }
-    else if (Convert.ToInt32(usrInput) == 7)
-    {
-        Console.WriteLine("Enter amount of hours you want to add: ");
-        usrInput = Console.ReadLine();
-        clock.addHour(Convert.ToInt32(usrInput));
-        Console.WriteLine($"Hour is now set to: {clock.getHour()}");
-    }
-    else if (Convert.ToInt32(usrInput) == 8)
-    {
-        Console.WriteLine("Enter amount of minutes you want to add: ");
-        usrInput = Console.ReadLine();
-        clock.addMinute(Convert.ToInt32(usrInput));
-        Console.WriteLine($"Minute is now set to: {clock.getMinute()}");
-    }
-    else if (Convert.ToInt32(usrInput) == 9)
-    {
-        Console.WriteLine("Enter amount of seconds you want to add: ");
-        usrInput = Console.ReadLine();
-        clock.addSecond(Convert.ToInt32(usrInput));
-        Console.WriteLine($"Second is now set to: {clock.getSecond()}");
-    }
-    else if (Convert.ToInt32(usrInput) == 10) {
-        Console.WriteLine(clock.ToString());
-    }
-    else if (Convert.ToInt32(usrInput) == 11) {
-        clock.tick();
-        Console.WriteLine("1 second has now been added");
-    }
-    else
-    {
-        Console.WriteLine("Not a valid operation, please try again.");
+        case "0":
+            Environment.Exit(0);
+            break;
+        case "1":
+        //prints currnet amount of hours on the clock
+            Console.WriteLine(clock.getHour() + " hour(s)");
+            break;
+        case "2":
+        //prints current amount of minutes on the clock
+            Console.WriteLine(clock.getMinute() + " minute(s)");
+            break;
+        case "3":
+        //prints current amount of seconds on the clock
+            Console.WriteLine(clock.getSecond() + " second(s)");
+            break;
+        case "5":
+        //here it accepts user input to set hour
+            Console.WriteLine("Enter hour you wish to set it to: ");
+            usrInput = Console.ReadLine();
+            clock.setHour(Convert.ToInt32(usrInput));
+            Console.WriteLine($"Hour is now set to: {clock.getHour()}");
+            break;
+        case "6":
+                //here it accepts user input to set minute
+            Console.WriteLine("Enter minute you wish to set it to: ");
+            usrInput = Console.ReadLine();
+            clock.setMinute(Convert.ToInt32(usrInput));
+            Console.WriteLine($"Minute is now set to: {clock.getMinute()}");
+            break;
+        case "7":
+                //here it accepts user input to set second
+            Console.WriteLine("Enter second you wish to set it to: ");
+            usrInput = Console.ReadLine();
+            clock.setSecond(Convert.ToInt32(usrInput));
+            Console.WriteLine($"Second is now set to: {clock.getSecond()}");
+            break;
+        case "8":
+        //here it accepts user input to add to hour
+            Console.WriteLine("Enter amount of hours you want to add: ");
+            usrInput = Console.ReadLine();
+            clock.addHour(Convert.ToInt32(usrInput));
+            Console.WriteLine($"Hour is now set to: {clock.getHour()}");
+            break;
+        case "9":
+        //here it accepts user input to add to minute
+            Console.WriteLine("Enter amount of minutes you want to add: ");
+            usrInput = Console.ReadLine();
+            clock.addMinute(Convert.ToInt32(usrInput));
+            Console.WriteLine($"Minute is now set to: {clock.getMinute()}");
+            break;
+        case "10":
+        //here it accepts user input to add to second
+            Console.WriteLine("Enter amount of seconds you want to add: ");
+            usrInput = Console.ReadLine();
+            clock.addSecond(Convert.ToInt32(usrInput));
+            Console.WriteLine($"Second is now set to: {clock.getSecond()}");
+            break;
+        case "4":
+        //Here it prints the time
+            Console.WriteLine(clock.ToString());
+            break;
+        case "11":
+        //Here it adds 1 second to the seconds
+            clock.tick();
+            Console.WriteLine("1 second has now been added");
+            break;
+        default:
+        //here it prints the error message
+            Console.WriteLine("Not a valid operation, please try again.");
+            break;
     }
 }
---
+++ Test/Program.cs
@@ -0,0 +1,32 @@
+﻿static void Main(string[] args)
+{
+    Test test = new Test();
+    test.TestEvent();
+    Console.ReadKey();
+}
+public class MyTest
+{
+    public event EventHandler MyEvent
+    {
+        add
+        {
+            Console.WriteLine("add operation");
+        }
+        remove
+        {
+            Console.WriteLine("remove operation");
+        }
+    }
+}
+public class Test
+{
+    public void TestEvent()
+    {
+        MyTest myTest = new MyTest();
+        myTest.MyEvent += myTest_MyEvent;
+        myTest.MyEvent -= myTest_MyEvent;
+    }
+    public void myTest_MyEvent(object sender, EventArgs e)
+    {
+    }
+}
